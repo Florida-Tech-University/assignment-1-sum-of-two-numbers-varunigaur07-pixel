d14ead78cc8cc9164f676630fa297ccf650f40c9
varunigaur07-pixel <varunigaur07@gmail.com>
2026-01-09 11:51:37 +0530

    Update Main.java
    Sum of a+b

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         // TODO: Read two integers
-      
+        int a = scaanner ,nextInt();
+        int b = scaanner ,nextInt();
+        int rslt = a+b;
 
         // TODO: Print their sum  using println
-      
+        System.out.println(rslt);
         scanner.close();
     }
 }
